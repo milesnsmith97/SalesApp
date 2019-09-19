d51fe09d6525cf106e79ca9708c55231ae507bad
milesnsmith97 <50618529+milesnsmith97@users.noreply.github.com>
2019-09-19 16:51:26 +0100

    Miles

--- sales-app/src/components/ChartSelect/ChartSelect.js
+++ sales-app/src/components/ChartSelect/ChartSelect.js
@@ -1,31 +1,44 @@
 import React from 'react';
+import './ChartSelect.css';
 
-import './ChartSelect.css'
-
-class ChartSelect extends React.Component {
-    state = {
-        defaultOption: '-- Select Chart Type --',
-    }
-
-    render() {
-        return <div>
-            {/* <div className='dropdown' style={{width: this.props.width || 280}}>
-                <select defaultValue={this.state.defaultOption}>
-                    <option value="">-- Select Chart Type --</option>
-                    <option value="option 1">Bar</option>
-                    <option selected value="option 2">Polar</option>
-                </select>
-            </div>
-        </div> */}
-        <div className="dropdown">
-            <button className="dropbtn">Filter By Date</button>
+const ChartSelect = props => (
+    <div className="dropdown">
+            <button className="dropbtn">
+                Filter By Category
+            </button>
             <div className="dropdown-content">
-                <a href="#">Polar</a>
-                <a href="#">Bear</a>
+                <a href ="#" onClick={props.click}>Wholesale</a>
+                <a href="#">Online</a>
+                <a href="#">Fol Deals</a>
             </div>
         </div>
-    </div>
-    }
-}
+);
+
+// class ChartSelect extends React.Component {
+//     state = {
+//         defaultOption: '-- Select Chart Type --',
+//     }
+
+//     render() {
+//         return <div>
+//             {/* <div className='dropdown' style={{width: this.props.width || 280}}>
+//                 <select defaultValue={this.state.defaultOption}>
+//                     <option value="">-- Select Chart Type --</option>
+//                     <option value="option 1">Bar</option>
+//                     <option selected value="option 2">Polar</option>
+//                 </select>
+//             </div>
+//         </div> */}
+//         <div className="dropdown">
+//             <button className="dropbtn">Filter By Category</button>
+//             <div className="dropdown-content">
+//                 <a href="#">Wholesale</a>
+//                 <a href="#">Online</a>
+//                 <a href="#">Fol Deals</a>
+//             </div>
+//         </div>
+//     </div>
+//     }
+// }
 
 export default ChartSelect;
